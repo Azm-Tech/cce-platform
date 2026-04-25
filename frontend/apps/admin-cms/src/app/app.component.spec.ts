9a406931953a6c3bf664934b1eb08c9389221b53
Moamen Kilany <m@M-Kilany-LP.local>
2026-04-25 23:00:52 +0300

    feat(phase-09): set Prettier config (singleQuote, trailingComma=all, 120 print width)

--- frontend/apps/admin-cms/src/app/app.component.spec.ts
+++ frontend/apps/admin-cms/src/app/app.component.spec.ts
@@ -14,9 +14,7 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain(
-      'Welcome admin-cms'
-    );
+    expect(compiled.querySelector('h1')?.textContent).toContain('Welcome admin-cms');
   });
 
   it(`should have as title 'admin-cms'`, () => {
