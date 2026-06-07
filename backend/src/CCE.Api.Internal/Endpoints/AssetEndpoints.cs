380ef68b02f9a6207a1ebdb1a5c2259c4c2f213f
ahmed <ahmed12348.com@gmail.com>
2026-06-07 14:40:59 +0300

    add download endpoint to internal api

--- backend/src/CCE.Api.Internal/Endpoints/AssetEndpoints.cs
+++ backend/src/CCE.Api.Internal/Endpoints/AssetEndpoints.cs
@@ -1,12 +1,16 @@
 using CCE.Api.Common.Extensions;
+using CCE.Application.Common.Interfaces;
+using CCE.Application.Content;
 using CCE.Application.Content.Commands.UploadAsset;
 using CCE.Application.Content.Queries.GetAssetById;
 using CCE.Domain;
+using CCE.Domain.Content;
 using CCE.Infrastructure;
 using MediatR;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Routing;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Options;
 
 namespace CCE.Api.Internal.Endpoints;
@@ -60,6 +64,32 @@ public static class AssetEndpoints
         })
         .RequireAuthorization(Permissions.Resource_Center_Upload)
         .WithName("GetAssetById");
+
+        assets.MapGet("/{id:guid}/download", async (
+            System.Guid id,
+            HttpContext httpContext,
+            ICceDbContext db,
+            IFileStorage storage,
+            CancellationToken ct) =>
+        {
+            var asset = await db.AssetFiles.FirstOrDefaultAsync(a => a.Id == id, ct).ConfigureAwait(false);
+            if (asset is null)
+                return Results.NotFound();
+
+            if (asset.VirusScanStatus != VirusScanStatus.Clean)
+                return Results.StatusCode(StatusCodes.Status403Forbidden);
+
+            httpContext.Response.ContentType = asset.MimeType;
+            httpContext.Response.Headers.ContentDisposition =
+                $"inline; filename=\"{System.Net.WebUtility.UrlEncode(asset.OriginalFileName)}\"";
+
+            await using var stream = await storage.OpenReadAsync(asset.Url, ct).ConfigureAwait(false);
+            await stream.CopyToAsync(httpContext.Response.Body, ct).ConfigureAwait(false);
+
+            return Results.Empty;
+        })
+        .RequireAuthorization(Permissions.Resource_Center_Upload)
+        .WithName("DownloadAsset");
 
         return app;
     }
